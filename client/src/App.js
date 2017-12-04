3a0e37337421aa51db76c52628d87ac5cf64602d
Carson Volker <carson@bright.md>
2017-12-04 08:26:19 -0800

    initial setup, servers running

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react'
+//import logo from './logo.svg';
+//import Letter from './components/Letter.js';
+//import Alphabet from './components/Alphabet.js';
+//import Board from './components/Board.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          {
+              <p>Welcome!</p>/*}<div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>*/}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
